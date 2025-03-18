3d4877db9776a0aba1637a9ae06d4d624718cb48
mangesh0410 <mangeshdighore@gmail.com>
2025-03-18 17:37:02 +0530

    adding files to pipeline

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css'; // Importing the CSS file for styles
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="text">Welcome to Code Build- Deploy Tool...</h1>
+    </div>
+  );
+};
+
+export default App;
